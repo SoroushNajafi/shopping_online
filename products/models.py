0272985b867e4fd9dc542bd47c3692c1ad1f9282
soroush <soroush.najafi89@gmail.com>
2022-09-17 12:59:46 +0430

    product model modified and productlistview created

--- products/models.py
+++ products/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from django.urls import reverse
 
 
-class Products(models.Model):
+class Product(models.Model):
+    CATEGORIES_CHOICES = (
+        ('women', 'women'),
+        ('men', 'men'),
+        ('kid', 'kid')
+    )
+
     title = models.CharField(max_length=200)
     description = models.TextField()
+    brand = models.CharField(max_length=200, blank=True)
+    categories = models.CharField(choices=CATEGORIES_CHOICES, max_length=200, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=6)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(ListView):
+    # model = Product
+    queryset = Product.objects.filter(active=True)
+    template_name = 'products/prodcust_list.html'
+    context_object_name = 'products'
