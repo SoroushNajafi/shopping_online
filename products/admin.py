dcfc739035a79fb2c1c46fe431658204fef0a237
soroush <soroush.najafi89@gmail.com>
2022-09-19 15:59:00 +0430

    comment model and form created and implemented

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'categories', 'brand', 'price', 'active')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('product', 'author', 'stars', 'active')
